fd26c17d419a8f338b4608d0dee13703324a959d
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-03-06 15:54:03 -0500

    Implement working token exchange

--- plaid_api/urls.py
+++ plaid_api/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'plaid_api'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_access_token', views.get_access_token),
 ]
--- plaid_api/views.py
+++ plaid_api/views.py
@@ -1,7 +1,10 @@
+import json
 import os
 import plaid
 
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
 PLAID_SECRET = os.getenv('PLAID_SECRET')
@@ -17,8 +20,11 @@ client = plaid.Client(
     api_version='2018-05-22',
 )
 
-# Create your views here.
+access_token = None
+
+
 def index(request):
+    """Show login landing page"""
     context = {
         'plaid_public_key': PLAID_PUBLIC_KEY,
         'plaid_environment': PLAID_ENV,
@@ -26,4 +32,31 @@ def index(request):
     }
     return render(request, "plaid_api/index.html", context)
 
+
+@require_http_methods(['POST', 'GET'])
+def get_access_token(request):
+    """Return plaid access token via POST only"""
+    global access_token
+    public_token = request.POST['public_token']
+    try:
+        exchange_response = client.Item.public_token.exchange(public_token)
+    except plaid.errors.PlaidError as e:
+        return jsonify(format_error(e))
+
+    pretty_print_response(exchange_response)
+    access_token = exchange_response['access_token']
+    return JsonResponse(exchange_response)
+
+def pretty_print_response(response):
+    print(json.dumps(response, indent=2, sort_keys=True))
+
+def format_error(e):
+    return {
+        'error': {
+            'display_message': e.display_message,
+            'error_code': e.code,
+            'error_type': e.type,
+            'error_message': e.message
+        }
+    }
 
