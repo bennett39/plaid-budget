176947dac80c84f44ebd3a1752080a6f065dee61
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-03-24 12:06:36 -0400

    Implement backend route to Angular

--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    return HttpResponse("Hello budget")
+    return render(request, "budget/index.html", context={})
